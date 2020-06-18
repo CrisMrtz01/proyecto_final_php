016910f0eb5946ea8644851b93a98f65f6283bf2
CrisMrtz01 <kooper8273@gmail.com>
2020-06-18 11:07:00 -0600

    Cambios hasta video 5

--- view/home.php
+++ view/home.php
@@ -3,7 +3,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
-  <title>Proyecto Final MVC PHP</title>
+  <title>Proyecto PHP-MVC-MYSQL</title>
 
   <!-- CSS  -->
   <link href="img/favicon.png" type="image/png" rel="icon"/>
@@ -12,22 +12,63 @@
   <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
 </head>
 <body>
-  <nav class="light-blue lighten-1" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo"><img src="img/logo.png" class="responsive-img"></a>
+  <nav id="nav" class="azul-ast" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="?c=<?php echo base64_encode('Home'); ?>" class="brand-logo"><img src="img/logo.png" class="responsive-img" ></a>
+    <!-- menú dispositivos grandes -->
       <ul class="right hide-on-med-and-down">
-        <li><a href="#">Navbar Link</a></li>
+        <li><a href="?c=<?php echo base64_encode('Talla'); ?>">Talla</a></li>
+        <li><a href="?c=<?php echo base64_encode('Color'); ?>">Color</a></li>
+        <li><a href="?c=<?php echo base64_encode('Cliente'); ?>">Cliente</a></li>
+        <li><a href="?c=<?php echo base64_encode('Usuario'); ?>">Usuario</a></li>
+
+        <!-- Dropdown Trigger -->
+        <li><a class="dropdown-trigger btn naranja-ast tooltipped" data-position="bottom" data-tooltip="Usuario activo" href='#' data-target='dropdown1'><i class="material-icons">person_pin</i></a></li>
+
+        <!-- Dropdown Structure -->
+        <ul id='dropdown1' class='dropdown-content'>
+          <li><a href="#" class="azul-ast white-text"><i class="material-icons white-text">menu</i>&nbsp;</a></li>
+          <li><a href="?c=<?php echo base64_encode('Login'); ?>&a=<?php echo base64_encode('Salir'); ?>" class="naranja-ast-text"><i class="material-icons">exit_to_app</i>Salir</li></a>
+          
+          <li><a href="?c=<?php echo base64_encode('Usuario'); ?>&a=<?php echo base64_encode('NuevaClave'); ?>" class="naranja-ast-text"><i class="material-icons">vpn_key</i>Nueva clave</a></li>
+          
+          <li><a href="?c=<?php echo base64_encode('Usuario'); ?>&a=<?php echo base64_encode('NuevaPregunta'); ?>" class="naranja-ast-text"><i class="material-icons">live_help</i>Nueva pregunta</a></li>
+
+          <li><a href="?c=<?php echo base64_encode('Usuario'); ?>&a=<?php echo base64_encode('EditarDatos'); ?>" class="naranja-ast-text"><i class="material-icons">assignment</i>Mis datos</a></li>
+        </ul>
       </ul>
 
-      <ul id="nav-mobile" class="sidenav">
-        <li><a href="#">Navbar Link</a></li>
+      <!-- menú dispositivos móbiles -->      
+      <ul id="nav-mobile" class="sidenav azul-ast">
+        <li><a class="white-text" href="?c=<?php echo base64_encode('Home'); ?>"><i class="material-icons white-text">home</i>inicio</a></li>
+        <li><a class=" white-text" href="?c=<?php echo base64_encode('Talla'); ?>"><i class="material-icons white-text">style</i>Talla</a></li>
+        <li><a class=" white-text" href="?c=<?php echo base64_encode('Color'); ?>"><i class="material-icons white-text">color_lens</i>Color</a></li>
+        <li><a class=" white-text" href="?c=<?php echo base64_encode('Cliente'); ?>"><i class="material-icons white-text">supervisor_account</i>Cliente</a></li>
+        <li><a class=" white-text" href="?c=<?php echo base64_encode('Usuario'); ?>"><i class="material-icons white-text">account_circle</i>Usuario</a></li>
+
+        <!-- Dropdown Trigger -->
+        <li><a class="dropdown-trigger btn naranja-ast" href='#' data-target='dropdown2'><i class="material-icons white-text">person_pin</i> Usuario activo</a></li>
+
+        <!-- Dropdown Structure -->
+        <ul id='dropdown2' class='dropdown-content'>
+          <li><a href="#" class="azul-ast white-text"><i class="material-icons white-text">menu</i>MENÚ</a></li>
+
+          <li><a href="?c=<?php echo base64_encode('Login'); ?>&a=<?php echo base64_encode('Salir'); ?>" class="naranja-ast-text"><i class="material-icons naranja-ast-text">exit_to_app</i>Salir</a></li>
+          
+          <li><a href="?c=<?php echo base64_encode('Usuario'); ?>&a=<?php echo base64_encode('NuevaClave'); ?>" class="naranja-ast-text"><i class="material-icons naranja-ast-text">vpn_key</i>Nueva clave</a></li>
+          
+          <li><a href="?c=<?php echo base64_encode('Usuario'); ?>&a=<?php echo base64_encode('NuevaPregunta'); ?>" class="naranja-ast-text"><i class="material-icons naranja-ast-text">live_help</i>Nueva pregunta</a></li>
+
+          <li><a href="?c=<?php echo base64_encode('Usuario'); ?>&a=<?php echo base64_encode('EditarDatos'); ?>" class="naranja-ast-text"><i class="material-icons naranja-ast-text">assignment</i>Mis datos</a></li>
+        </ul>
       </ul>
       <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     </div>
   </nav>
+  
   <div class="section no-pad-bot" id="index-banner">
     <div class="container">
       <br><br>
-      <h1 class="header center orange-text">Proyecto Final</h1>
+      <h1 class="header center orange-text">Proyecto final</h1>
       <div class="row center">
         <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
       </div>
@@ -77,38 +118,44 @@
     <br><br>
   </div>
 
-  <footer class="page-footer orange">
+  <footer id="footer" class="page-footer naranja-ast">
     <div class="container">
+      <!-- floating button -->
+      <div class="fixed-action-btn">
+        <a class="btn-floating btn-large azul-ast">
+          <i class="large material-icons">airplay</i>
+        </a>
+        <ul>
+          <li><a href="#footer" class="btn-floating gris-ast"><i class="material-icons">arrow_downward</i></a></li>
+          <li><a href="#nav" class="btn-floating gris-ast"><i class="material-icons">arrow_upward</i></a></li>
+        </ul>
+      </div>
+      <!-- foot -->
       <div class="row">
         <div class="col l6 s12">
-          <h5 class="white-text">Company Bio</h5>
-          <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
-
+          <h5 class="white-text">Proyecto final</h5>
+          <p class="grey-text text-lighten-4">
+            Proyecto final desarrollado en PHP, con la metodología MVC (Model - View - Controller) y MySQL como gestor de bases de datos. 
+            Todas las vistas se crearon con el framework de diseño Materializecss basado en Material Design.
+          </p>
 
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Settings</h5>
+          <h5 class="white-text">Contactos</h5>
           <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
+            <li><a class="white-text" href="mailto:amakakeo@gmail.com">email: amakakeo@gmail.com</a></li>
+            <li><a class="white-text" href="https://www.youtube.com/channel/UCdCd9f4XTh2sle8P3oT_xYg" target="_blank">Youtube: Armando Sánchez Trabajando</a></li>
+            <li><a class="white-text" href="www.facebook.com/armandosanchez.trabajando" target="_blank">Facebook: Armando Sánchez Trabajando</a></li>
           </ul>
         </div>
         <div class="col l3 s12">
-          <h5 class="white-text">Connect</h5>
-          <ul>
-            <li><a class="white-text" href="#!">Link 1</a></li>
-            <li><a class="white-text" href="#!">Link 2</a></li>
-            <li><a class="white-text" href="#!">Link 3</a></li>
-            <li><a class="white-text" href="#!">Link 4</a></li>
-          </ul>
+          <img src="img/logo.png" alt="Logo" class="responsive-img">
         </div>
       </div>
     </div>
-    <div class="footer-copyright">
+    <div class="footer-copyright gris-ast">
       <div class="container">
-      Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+      by <a class="white-text" href="https://www.youtube.com/channel/UCdCd9f4XTh2sle8P3oT_xYg" target="_blank">Ing. Armando Sánchez | Docente de educación técnica y superior en computación.</a>
       </div>
     </div>
   </footer>
